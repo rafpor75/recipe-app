132f6a7df0321cb0b3eed4b64f76ecf4e5dc917b
Raffaele Porzio <r.porzio@unimarconi.it>
2018-03-08 16:20:04 +0100

    Added one to one relation

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,14 @@
+package guru.springframework.domain;
+
+public class UnitOfMeasure {
+	private String uom;
+
+	public String getUom() {
+		return uom;
+	}
+
+	public void setUom(String uom) {
+		this.uom = uom;
+	}
+	
+}
